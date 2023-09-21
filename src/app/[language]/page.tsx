d3e0c396ab30bba38abb8d2b55589e699e1db79c
Vladyslav Shchepotin <vladislav.schepotin@gmail.com>
2023-09-21 22:06:50 +0300

    feat: add description to home page

--- src/app/[language]/page.tsx
+++ src/app/[language]/page.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
-import Home from "./page-content";
 import { getServerTranslation } from "@/services/i18n";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import MuiLink from "@mui/material/Link";
+import { Trans } from "react-i18next/TransWithoutContext";
 
 type Props = {
   params: { language: string };
@@ -14,4 +18,34 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
-export default Home;
+export default async function Home({ params }: Props) {
+  const { t } = await getServerTranslation(params.language, "home");
+
+  return (
+    <Container maxWidth="md">
+      <Grid container spacing={3} wrap="nowrap" pt={3}>
+        <Grid item>
+          <Typography variant="h3" gutterBottom>
+            {t("title")}
+          </Typography>
+          <Typography>
+            <Trans
+              i18nKey={`description`}
+              t={t}
+              components={[
+                <MuiLink
+                  key="1"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/brocoders/react-boilerplate"
+                >
+                  {}
+                </MuiLink>,
+              ]}
+            />
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
