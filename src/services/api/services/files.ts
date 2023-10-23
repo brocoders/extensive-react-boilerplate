08b210bdf3ca123927e91b05f12c2ae07dc9cda7
Vladyslav Shchepotin <vladislav.schepotin@gmail.com>
2023-10-24 00:16:53 +0300

    feat: add AbortController to API services

--- src/services/api/services/files.ts
+++ src/services/api/services/files.ts
@@ -3,6 +3,7 @@ import useFetch from "../use-fetch";
 import { API_URL } from "../config";
 import wrapperFetchJsonResponse from "../wrapper-fetch-json-response";
 import { FileEntity } from "../types/file-entity";
+import { RequestConfigType } from "./types/request-config";
 
 export type FileUploadRequest = File;
 
@@ -12,13 +13,14 @@ export function useFileUploadService() {
   const fetch = useFetch();
 
   return useCallback(
-    (data: FileUploadRequest) => {
+    (data: FileUploadRequest, requestConfig?: RequestConfigType) => {
       const formData = new FormData();
       formData.append("file", data);
 
       return fetch(`${API_URL}/v1/files/upload`, {
         method: "POST",
         body: formData,
+        ...requestConfig,
       }).then(wrapperFetchJsonResponse<FileUploadResponse>);
     },
     [fetch]
