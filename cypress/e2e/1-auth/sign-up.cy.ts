2599456e1eb088b61faacf0e10fb2b66c530fc2e
Vladyslav Shchepotin <vladislav.schepotin@gmail.com>
2023-12-25 18:21:17 +0200

    fix: prettier

--- cypress/e2e/1-auth/sign-up.cy.ts
+++ cypress/e2e/1-auth/sign-up.cy.ts
@@ -24,55 +24,55 @@ describe("Sign Up", () => {
       email,
       password: nanoid(),
       firstName: `FirstName${nanoid()}`,
-      lastName: `LastName${nanoid()}`
-    })
+      lastName: `LastName${nanoid()}`,
+    });
 
     cy.getBySel("firstName").type(`FirstName${nanoid()}`);
     cy.getBySel("lastName").type(`LastName${nanoid()}`);
     cy.getBySel("email").type(email);
     cy.getBySel("password").type(nanoid());
     cy.getBySel("sign-up-submit").click();
-    cy.getBySel("email-error").should("be.visible")
+    cy.getBySel("email-error").should("be.visible");
   });
 
   it("Check validation of rquired fields errors", () => {
     cy.getBySel("sign-up-submit").click();
-    cy.getBySel("firstName-error").should("be.visible")
-    cy.getBySel("lastName-error").should("be.visible")
-    cy.getBySel("email-error").should("be.visible")
-    cy.getBySel("password-error").should("be.visible")
+    cy.getBySel("firstName-error").should("be.visible");
+    cy.getBySel("lastName-error").should("be.visible");
+    cy.getBySel("email-error").should("be.visible");
+    cy.getBySel("password-error").should("be.visible");
 
-    cy.getBySel("firstName").type(`FirstName${nanoid()}`)
-    cy.getBySel("firstName-error").should("not.exist")
+    cy.getBySel("firstName").type(`FirstName${nanoid()}`);
+    cy.getBySel("firstName-error").should("not.exist");
 
-    cy.getBySel("lastName").type(`LastName${nanoid()}`)
-    cy.getBySel("lastName-error").should("not.exist")
+    cy.getBySel("lastName").type(`LastName${nanoid()}`);
+    cy.getBySel("lastName-error").should("not.exist");
 
-    cy.getBySel("email").type(`test${nanoid()}@example.com`)
-    cy.getBySel("email-error").should("not.exist")
+    cy.getBySel("email").type(`test${nanoid()}@example.com`);
+    cy.getBySel("email-error").should("not.exist");
 
-    cy.getBySel("password").type(nanoid())
-    cy.getBySel("password-error").should("not.exist")
+    cy.getBySel("password").type(nanoid());
+    cy.getBySel("password-error").should("not.exist");
 
-    cy.getBySel("sign-up-submit").click()
+    cy.getBySel("sign-up-submit").click();
     cy.location("pathname").should("not.include", "/sign-up");
-  })
+  });
 
   it("Check validation for password", () => {
     cy.getBySel("firstName").type(`FirstName${nanoid()}`);
     cy.getBySel("lastName").type(`LastName${nanoid()}`);
     cy.getBySel("email").type(`test${nanoid()}@example.com`);
 
-    cy.getBySel("password").type('p{enter}');
+    cy.getBySel("password").type("p{enter}");
     cy.getBySel("password-error").should("be.visible");
 
-    cy.getBySel("password").type('assw{enter}');
+    cy.getBySel("password").type("assw{enter}");
     cy.getBySel("password-error").should("be.visible");
 
-    cy.getBySel("password").type('1{enter}');
+    cy.getBySel("password").type("1{enter}");
     cy.getBySel("password-error").should("not.exist");
 
-    cy.getBySel("password").type('{backspace}');
+    cy.getBySel("password").type("{backspace}");
     cy.getBySel("password-error").should("be.visible");
-  })
+  });
 });
