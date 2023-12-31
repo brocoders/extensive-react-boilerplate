eb5b7669bbf0f7f50b614d8ad7df3f249e9cd427
Vladyslav Shchepotin <vladislav.schepotin@gmail.com>
2023-12-31 18:50:32 +0200

    feat(cy): receive emails by IMAP

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
 import { defineConfig } from "cypress";
-require('dotenv').config()
-require('dotenv').config({ path: ".env.local"})
+import { getLatestEmail } from "./cypress/support/helpers/email";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents() {
-      // implement node event listeners here
+    setupNodeEvents(on) {
+      on("task", {
+        "mail:receive": async () => getLatestEmail(),
+      });
     },
     // Need for waiting api server
     defaultCommandTimeout: 60000,
@@ -15,5 +18,5 @@ export default defineConfig({
   },
   env: {
     apiUrl: process.env.NEXT_PUBLIC_API_URL,
-  }
+  },
 });
