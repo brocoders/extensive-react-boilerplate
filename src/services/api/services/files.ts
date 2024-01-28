08350b540ad47f2352636b74ee3567282eaf504e
Vladyslav Shchepotin <vladislav.schepotin@gmail.com>
2024-01-29 00:33:19 +0200

    feat: add presigned s3 upload

--- src/services/api/services/files.ts
+++ src/services/api/services/files.ts
@@ -4,25 +4,52 @@ import { API_URL } from "../config";
 import wrapperFetchJsonResponse from "../wrapper-fetch-json-response";
 import { FileEntity } from "../types/file-entity";
 import { RequestConfigType } from "./types/request-config";
+import HTTP_CODES_ENUM from "../types/http-codes";
 
 export type FileUploadRequest = File;
 
-export type FileUploadResponse = FileEntity;
+export type FileUploadResponse = {
+  file: FileEntity;
+  uploadSignedUrl: string;
+};
 
 export function useFileUploadService() {
-  const fetch = useFetch();
+  const fetchClient = useFetch();
 
   return useCallback(
-    (data: FileUploadRequest, requestConfig?: RequestConfigType) => {
-      const formData = new FormData();
-      formData.append("file", data);
+    async (data: FileUploadRequest, requestConfig?: RequestConfigType) => {
+      if (process.env.NEXT_PUBLIC_FILE_DRIVER === "s3-presigned") {
+        const result = await fetchClient(`${API_URL}/v1/files/upload`, {
+          method: "POST",
+          body: JSON.stringify({
+            fileName: data.name,
+            fileSize: data.size,
+          }),
+          ...requestConfig,
+        }).then(wrapperFetchJsonResponse<FileUploadResponse>);
 
-      return fetch(`${API_URL}/v1/files/upload`, {
-        method: "POST",
-        body: formData,
-        ...requestConfig,
-      }).then(wrapperFetchJsonResponse<FileUploadResponse>);
+        if (result.status === HTTP_CODES_ENUM.CREATED) {
+          await fetch(result.data.uploadSignedUrl, {
+            method: "PUT",
+            body: data,
+            headers: {
+              "Content-Type": data.type,
+            },
+          });
+        }
+
+        return result;
+      } else {
+        const formData = new FormData();
+        formData.append("file", data);
+
+        return fetchClient(`${API_URL}/v1/files/upload`, {
+          method: "POST",
+          body: formData,
+          ...requestConfig,
+        }).then(wrapperFetchJsonResponse<FileUploadResponse>);
+      }
     },
-    [fetch]
+    [fetchClient]
   );
 }
