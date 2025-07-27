de357891b744547828b5e34d9e096595b849481c
Vladyslav Shchepotin <vladislav.schepotin@gmail.com>
2025-07-27 23:40:23 +0300

    chore(deps): storybook to v9

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,9 +5,8 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    "@storybook/addon-docs"
   ],
   framework: {
     name: "@storybook/nextjs",
