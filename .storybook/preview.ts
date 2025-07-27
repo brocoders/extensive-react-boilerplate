39a9f636a88fb335ab9e0609054e084f79ba7e9e
Vladyslav Shchepotin <vladislav.schepotin@gmail.com>
2025-07-27 23:40:23 +0300

    chore(deps): storybook to v9

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,4 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from "@storybook/nextjs";
 
 const preview: Preview = {
   parameters: {
--- src/components/form/checkbox-boolean/form-checkbox-boolean.stories.tsx
+++ src/components/form/checkbox-boolean/form-checkbox-boolean.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Meta, StoryFn } from "@storybook/react";
+import { Meta, StoryFn } from "@storybook/nextjs";
 import { useForm, FormProvider } from "react-hook-form";
 import FormCheckboxBooleanInput, {
   CheckboxBooleanInputProps,
