c64b62265b52f00e6486ba01f5bc87be6a4e9630
Vladyslav Shchepotin <vladislav.schepotin@gmail.com>
2026-03-19 15:44:03 +0200

    chore(deps): update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    dirs: ["src", "playwright-tests"],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
--- src/services/social-auth/google/google-auth-provider.tsx
+++ src/services/social-auth/google/google-auth-provider.tsx
@@ -2,10 +2,13 @@
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { isGoogleAuthEnabled, googleClientId } from "./google-config";
+import useLanguage from "@/services/i18n/use-language";
 
 function GoogleAuthProvider({ children }: { children: React.ReactNode }) {
+  const language = useLanguage();
+
   return isGoogleAuthEnabled && googleClientId ? (
-    <GoogleOAuthProvider clientId={googleClientId}>
+    <GoogleOAuthProvider clientId={googleClientId} locale={language}>
       {children}
     </GoogleOAuthProvider>
   ) : (
